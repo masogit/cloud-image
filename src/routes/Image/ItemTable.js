4ef825a10072cfc1c514c064dabd0fd2b05355bf
满硕 <shuo.man@gmail.com>
2018-03-30 10:37:36 +0800

    add plus icon

--- src/routes/Image/ItemTable.js
+++ src/routes/Image/ItemTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Icon, Modal, Card } from 'antd';
+import { Table, Icon, Modal } from 'antd';
 import { Images, Conclusion } from './Reports';
 import DescriptionList from '../../components/DescriptionList';
 
@@ -67,18 +67,28 @@ class ItemTable extends Component {
         <br />
         <DescriptionList size="small" col="1">
           <Description term="报告所见">
-            <p>
-              患者因突起右侧肢体活动不利、失语20天入院。入院前曾在外院住院治疗15天，5天前入住我科。20天前头部CT示“左侧颞顶叶大面积脑梗塞”。
-              <br />
-              <br />
-              入院后给予中医辨证治疗，病情好转。现患者生命体征稳定，神志清楚，言语含糊不清，右侧鼻唇沟变浅，口角稍左歪，两肺呼吸音清；心率89次/分，律齐，心音可；肝脾未扪及。
-              <br />
-              <br />
-              右上下肢肌力均Ⅱ级，肌张力稍高，右侧膝反射亢进、Babinski征阳性；脑膜刺激征阴性。患者同意配合针灸治疗，特申请贵科会诊协助针灸治疗。
-            </p>
+            <Flex>
+              <p>
+                患者因突起右侧肢体活动不利、失语20天入院。入院前曾在外院住院治疗15天，5天前入住我科。20天前头部CT示“左侧颞顶叶大面积脑梗塞”。
+                <br />
+                <br />
+                入院后给予中医辨证治疗，病情好转。现患者生命体征稳定，神志清楚，言语含糊不清，右侧鼻唇沟变浅，口角稍左歪，两肺呼吸音清；心率89次/分，律齐，心音可；肝脾未扪及。
+                <br />
+                <br />
+                右上下肢肌力均Ⅱ级，肌张力稍高，右侧膝反射亢进、Babinski征阳性；脑膜刺激征阴性。患者同意配合针灸治疗，特申请贵科会诊协助针灸治疗。
+              </p>
+              <a href="">
+                <Icon type="plus-circle-o" />
+              </a>
+            </Flex>
           </Description>
           <Description term="报告结论">
-            <b>未见异常</b>
+            <Flex>
+              <b>未见异常</b>
+              <a href="">
+                <Icon type="plus-circle-o" />
+              </a>
+            </Flex>
           </Description>
         </DescriptionList>
       </div>
@@ -86,4 +96,7 @@ class ItemTable extends Component {
   }
 }
 
+const Flex = props => (
+  <div style={{ display: 'flex', justifyContent: 'space-between' }}>{props.children}</div>
+);
 export default ItemTable;
